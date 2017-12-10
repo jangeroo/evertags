3a80801a619a44ddabf3a1ad34aed0c6d2841c97
Michael Jang <michaeljang86@gmail.com>
2017-12-10 11:52:45 -0500

    Connected server to Evernote

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 
+const Evernote = require('evernote');
+var client = new Evernote.Client({ token: "S=s1:U=91d19:E=167821b5a16:C=1602a6a2aa8:P=1cd:A=en-devtoken:V=2:H=2e45e5c070baf4f3980f7c351f64de1e" });
+var userStore = client.getUserStore();
+// userStore.getUser().catch(error => console.log(error))
+userStore.getUser()
+  .then(user => console.log(user))
+  .catch(error => console.log(error))
+var noteStore = client.getNoteStore()
+noteStore.listTags()
+  .then(tags => console.log(tags))
+  .catch(error => console.log(error))
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Evertags API server!')
