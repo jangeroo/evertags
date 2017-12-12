17895b73d9efd48471f463c1d8dd5f94d9e52650
Michael Jang <michaeljang86@gmail.com>
2017-12-12 15:55:44 -0500

    Added /notes endpoint to server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const config = require('./src/enOAuthConfig.json')
 const Evernote = require('evernote');
 var client = new Evernote.Client({ token: config.token });
-var userStore = client.getUserStore();
+var noteStore = client.getNoteStore()
 
 
 const cors = (req, res, next) => {
@@ -19,12 +19,32 @@ app.get('/', (req, res) => {
 })
 
 app.get('/tags', (req, res) => {
-  var noteStore = client.getNoteStore()
   noteStore.listTags()
     .then(tags => res.json(tags))
     .catch(error => console.log(error))
 })
 
+app.get('/notes', (req, res) => {
+  if (!req.query.tagGuid) {
+    res.json({
+      code: 400,
+      status: "Bad Request",
+      message: "Missing tagGuid"
+    })
+  }
+  let filter = new Evernote.NoteStore.NoteFilter({
+    tagGuids: [req.query.tagGuid]
+  })
+
+  let spec = new Evernote.NoteStore.NotesMetadataResultSpec({
+    includeTitle: true
+  })
+
+  noteStore.findNotesMetadata(filter, 0, 100, spec)
+    .then(results => res.json(results.notes))
+    .catch(error => console.log(error))
+
+})
 
 const PORT = 4000
 app.listen(PORT, () => {
