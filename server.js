6eebf80aa4d50b61aa9e1217ea69c5fe674ae3b4
Michael Jang <michaeljang86@gmail.com>
2017-12-12 13:11:14 -0500

    Added cors handling to server

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 
 const config = require('./src/enOAuthConfig.json')
-console.log(config)
 const Evernote = require('evernote');
 var client = new Evernote.Client({ token: config.token });
 var userStore = client.getUserStore();
+
+
+const cors = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  next()
+}
+app.use(cors)
 
 
 app.get('/', (req, res) => {
@@ -19,29 +25,8 @@ app.get('/tags', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
 const PORT = 4000
 app.listen(PORT, () => {
   console.log('Evertags server listening on port', PORT)
 })
-
-
-mockTags = [
-  {
-    'updateSequenceNum': '01',
-    'guid': 'tag_001',
-    'name': '#booya',
-    'parentGuid': null,
-  },
-  {
-    'updateSequenceNum': '02',
-    'guid': 'tag_001',
-    'name': '#wahoo',
-    'parentGuid': null,
-  },
-  {
-    'updateSequenceNum': '03',
-    'guid': 'tag_003',
-    'name': '#BAM',
-    'parentGuid': 'tag_001',
-  },
-]
